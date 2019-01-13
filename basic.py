a88a1e9c17447c35c5e7fe517e55c6418b008c2d
first <2679411235@qq.com>
2019-01-13 16:23:14 +0800

    v3

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 import scrapy
+from properties.items import PropertiesItem
 
 class BasicSpider(scrapy.Spider):
 	name="basic"
@@ -6,8 +7,10 @@ class BasicSpider(scrapy.Spider):
 	start_urls=('http://web:9312/properties/property_000000.html',)
 	
 	def parse(self,response):
-		self.log("title: %s" % response.xpath('//*[@itemprop="name"][1]/text()').extract())
-		self.log("price: %s" % response.xpath('//*[@itemprop="price"][1]/text()').re('[.0-9]+'))
-		self.log("description: %s" % response.xpath('//*[@itemprop="description"][1]/text()').extract())
-		self.log("address: %s" % response.xpath('//*[@itemtype="http://schema.org/Place"][1]/text()').extract())
-		self.log("image_urls: %s" % response.xpath('//*[@itemprop="image"][1]/@src').extract())
+		item=PropertiesItem()
+		item['title']=response.xpath('//*[@itemprop="name"][1]/text()').extract())
+		item['price']=response.xpath('//*[@itemprop="price"][1]/text()').re('[.0-9]+'))
+		item['description']=response.xpath('//*[@itemprop="description"][1]/text()').extract())
+		item['address']=response.xpath('//*[@itemtype="http://schema.org/Place"][1]/text()').extract())
+		item['image_URL']=response.xpath('//*[@itemprop="image"][1]/@src').extract())
+		return item
